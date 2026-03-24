ada5ecf7286f2e9ca11037b9163144fb8459a68c
Miguel Dagraza <migueldagraza@gmail.com>
2026-03-24 13:52:07 +0100

    feat: update authentication logic to include user role and swagger authetication with RequestForm

--- app/core/security.py
+++ app/core/security.py
@@ -8,7 +8,7 @@ import os
 
 load_dotenv()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login") #https://fastapi.tiangolo.com/es/tutorial/security/simple-oauth2/ | https://cosasdedevs.com/posts/autenticacion-login-jwt-fastapi/
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token") #https://fastapi.tiangolo.com/es/tutorial/security/simple-oauth2/ | https://cosasdedevs.com/posts/autenticacion-login-jwt-fastapi/
 
 pwd_context = CryptContext(
     schemes=["bcrypt"],
@@ -24,7 +24,7 @@ def verify_password(plain: str, hashed: str) -> bool:
 def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes=5)): #https://www.rfc-editor.org/rfc/rfc7519#section-4.1.2
     to_encode = data.copy()
     expire = datetime.now(timezone.utc) + (expires_delta)
-    to_encode.update({"exp": expire})
+    to_encode.update({"exp": expire, "iat": datetime.now(timezone.utc)})
     encoded_jwt = jwt.encode(to_encode, os.getenv("SECRET_KEY"))
     return encoded_jwt
 
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -8,6 +8,7 @@ from app.models.user import User
 from app.schemas.auth import UserLogin, UserResponse
 from app.core.security import verify_password, create_access_token, get_username
 from datetime import timedelta
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -32,7 +33,34 @@ async def auth_login(auth_data: UserLogin, db: AsyncSession = Depends(get_db)):
         )
     
     access_token = create_access_token(
-        data={"sub": user.username},
+        data={"sub": user.username, "role": user.role.value},
+        expires_delta=timedelta(minutes=30)
+    )
+
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@router.post("/token", include_in_schema=False) #TODO: Endpoint duplicado, extraer logica a otra función o revisar mezcla | Usado para login Swagger
+async def auth_login(auth_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)):
+    result = await db.execute(
+        select(User).where(User.username == auth_data.username)
+    )
+
+    user = result.scalar_one_or_none()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Credenciales inválidas"
+        )
+
+    if not verify_password(auth_data.password, user.passwordHash):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Credenciales inválidas"
+        )
+    
+    access_token = create_access_token(
+        data={"sub": user.username, "role": user.role.value},
         expires_delta=timedelta(minutes=30)
     )
 
